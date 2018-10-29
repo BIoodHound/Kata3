c0d716c7a907c5b072630bb52a711d6bc413590b
gonca <gonca@DESKTOP-UT10VI0>
2018-10-29 22:09:51 +0000

    Kata 3 versión 2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,10 +3,32 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
+        Histogram<String> histogram = new Histogram<>();
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.com");
+        histogram.increment("ulpgc.com");
+        histogram.increment("ulpgc.com");
+        histogram.increment("ulpgc.com");
+        histogram.increment("ulpgc.com");
+        histogram.increment("ull.com");
+        histogram.increment("ull.com");
+        histogram.increment("ull.com");
+        histogram.increment("ull.com");
         
         
-        
-        HistogramDisplay histo = new HistogramDisplay();
+        HistogramDisplay histo = new HistogramDisplay(histogram);
         histo.execute();
     }
     
